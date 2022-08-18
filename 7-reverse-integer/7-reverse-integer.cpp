04bb04aed012581f0653f3cdaf4c2456b0bb6d6c
Vijay Manikanta Reddy Sathi <83952736+vijaymanikantareddy@users.noreply.github.com>
2022-08-18 21:56:19 +0530

    Time: 3 ms (64.86%), Space: 5.9 MB (31.64%) - LeetHub

---
+++ 7-reverse-integer/7-reverse-integer.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int reverse(int n) {
+        int sum=0;
+        while(n){
+            int d = n%10;
+            if(sum<INT_MIN/10 or sum>INT_MAX/10)
+                return 0;
+            sum = sum*10 + d;
+            n/=10;
+        }
+        return sum;
+    }
+};
