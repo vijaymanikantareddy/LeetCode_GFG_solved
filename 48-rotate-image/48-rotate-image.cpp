a683a158f7db759ab93860f212bfd7aeb5070248
Vijay Manikanta Reddy Sathi <83952736+vijaymanikantareddy@users.noreply.github.com>
2022-09-10 06:53:25 +0530

    Time: 4 ms (41.97%), Space: 7.1 MB (73.72%) - LeetHub

---
+++ 48-rotate-image/48-rotate-image.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        for(int i=1 ; i<matrix.size() ; i++){
+            for(int j=0 ; j<=i ; j++){
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+        for(int i=0 ; i<matrix.size() ; i++){
+            reverse(matrix[i].begin(), matrix[i].end());
+        }
+    }
+};
