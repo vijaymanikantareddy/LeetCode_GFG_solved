bb97be678fce50c91f0c62f6c702fa863be7694f
Vijay Manikanta Reddy Sathi <83952736+vijaymanikantareddy@users.noreply.github.com>
2022-10-18 06:57:00 +0530

    Added solution - LeetHub

---
+++ Bitonic Point - GFG/bitonic-point.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:
+	
+	int findMaximum(int arr[], int n) {
+	    // code here
+	    int l=0, r=n-1, mx = arr[0];
+	    while(l<=r){
+	        mx = arr[l]>mx? arr[l]: mx;
+	        mx = arr[r]>mx? arr[r]: mx;
+	        l++;
+	        r--;
+	    }
+	    return mx;
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, i;
+        cin >> n;
+        int arr[n];
+        for (i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.findMaximum(arr, n);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
