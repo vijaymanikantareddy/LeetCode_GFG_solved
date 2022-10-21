931f268cccd638a6357837c111c30f5724fb7d13
Vijay Manikanta Reddy Sathi <83952736+vijaymanikantareddy@users.noreply.github.com>
2022-10-21 18:13:36 +0530

    Added solution - LeetHub

---
+++ Binary Shift to Divide - GFG/binary-shift-to-divide.cpp
@@ -0,0 +1,29 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution{
+    public:
+    int half(int N){
+        // code here
+        return N==1? N : N>>1;
+    }
+};
+
+//{ Driver Code Starts.
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin>>N;
+        
+        Solution ob;
+        cout<<ob.half(N)<<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
