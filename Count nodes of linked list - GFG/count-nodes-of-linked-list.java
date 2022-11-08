3860fc4a3cedeb4d981c4a2a786f85c915de8473
Vijay Manikanta Reddy Sathi <83952736+vijaymanikantareddy@users.noreply.github.com>
2022-11-08 21:48:14 +0530

    Added solution - LeetHub

---
+++ Count nodes of linked list - GFG/count-nodes-of-linked-list.java
@@ -0,0 +1,69 @@
+//{ Driver Code Starts
+import java.util.*;
+class Node{
+    int data;
+    Node next;
+    
+    Node(int a){
+        data = a;
+        next = null;
+    }
+    
+}
+
+// } Driver Code Ends
+/*Complete the function below*/
+/*
+class Node{
+    int data;
+    Node next;
+    Node(int a){  data = a; next = null; }
+}*/
+
+class Solution
+{
+    //Function to count nodes of a linked list.
+    public static int getCount(Node head)
+    {
+        
+        //Code here
+        int cnt = 0;
+        Node temp = head;
+        while(temp != null){
+            cnt++;
+            temp = temp.next;
+        }
+        return cnt;
+    }
+}
+    
+
+//{ Driver Code Starts.
+class LinkedList{
+    
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0)
+        {
+            int n=sc.nextInt();
+            Node head = new Node(sc.nextInt());
+            Node tail = head;
+            for(int i=0; i<n-1; i++)
+            {
+                tail.next = new Node(sc.nextInt());
+                tail = tail.next;
+            }
+            
+            Solution ob = new Solution();
+            System.out.println(ob.getCount(head));
+        }
+    }
+   
+    
+    
+    
+}
+
+
+// } Driver Code Ends
