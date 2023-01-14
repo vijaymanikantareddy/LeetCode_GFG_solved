86b14679100c9af1f2e9befdb9498ba1331dbe5b
Vijay Manikanta Reddy Sathi <83952736+vijaymanikantareddy@users.noreply.github.com>
2023-01-14 16:55:32 +0530

    Added solution - LeetHub

---
+++ Check an Integer is power of 3 or not - GFG/check-an-integer-is-power-of-3-or-not.cpp
@@ -0,0 +1,28 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+class Solution{   
+public:
+    string isPowerof3(int N){
+        // code here 
+        return (1162261467%N == 0)? "Yes" : "No";
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin >> N;
+        Solution ob;
+        cout << ob.isPowerof3(N) << endl;
+    }
+    return 0; 
+} 
+// } Driver Code Ends
