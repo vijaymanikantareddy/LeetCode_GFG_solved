0305e2334ea408e6bd4a928ee169f1a0a7eb4a82
Vijay Manikanta Reddy Sathi <83952736+vijaymanikantareddy@users.noreply.github.com>
2023-01-18 23:32:30 -0500

    Added solution - LeetHub

---
+++ Pattern 5 - GFG/pattern-5.java
@@ -0,0 +1,33 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Main {
+    // Driver code
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine().trim());
+            Solution obj=new Solution();
+            obj.printTriangle(n);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+
+    void printTriangle(int n) {
+        // code here
+        int t= n;
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < t ; j++){
+                System.out.print("* ");
+            }
+            t--;
+            System.out.println();
+        }
+    }
+}
