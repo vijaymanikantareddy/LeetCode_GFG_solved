1ffc5440586829a1cbc4d8ab40b0eea75b27b083
Vijay Manikanta Reddy Sathi <83952736+vijaymanikantareddy@users.noreply.github.com>
2023-01-21 21:47:23 +0530

    Added solution - LeetHub

---
+++ The New Line - GFG/the-new-line.cpp
@@ -0,0 +1,30 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<iostream>
+using namespace std;
+
+void printNewLine();
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+// Function to print each word in new line
+void printNewLine(){
+    
+    // Your code here
+    cout<<"Geeks\nfor\nGeeks";
+    
+}
+
+//{ Driver Code Starts.
+
+int main(){
+    
+    //function call
+    printNewLine();
+    
+}
+
+// } Driver Code Ends
