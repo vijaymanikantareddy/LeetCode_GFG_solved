805afc8a39a46e7de38ce32c007d0092c05c55b6
Vijay Manikanta Reddy Sathi <83952736+vijaymanikantareddy@users.noreply.github.com>
2023-01-21 19:33:43 +0530

    Added solution - LeetHub

---
+++ Print GFG n times - GFG/print-gfg-n-times.java
@@ -0,0 +1,32 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Main {
+    // Driver code
+    public static void main(String[] args) throws Exception {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine().trim());
+            Solution obj = new Solution();
+            obj.printGfg(n);
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+
+    void printGfg(int N) {
+        // code here
+        if(N == 0){
+            return ;
+        }
+        System.out.print("GFG ");
+        printGfg(N-1);
+    }
+}
