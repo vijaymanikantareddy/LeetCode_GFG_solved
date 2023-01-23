8b7a2fd13ab3fb1ec5ddc47098ae45c6c4754c6a
Vijay Manikanta Reddy Sathi <83952736+vijaymanikantareddy@users.noreply.github.com>
2023-01-23 07:47:10 +0530

    Added solution - LeetHub

---
+++ Preorder traversal (Iterative) - GFG/preorder-traversal-iterative.java
@@ -0,0 +1,139 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.io.*;
+import java.util.*;
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+class GfG {
+    
+    static Node buildTree(String str){
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+  
+	public static void main (String[] args) throws IOException{
+	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        
+	        int t=Integer.parseInt(br.readLine());
+    
+	        while(t > 0){
+	            String s = br.readLine();
+    	    	Node root = buildTree(s);
+    	    	Tree g = new Tree();
+                ArrayList<Integer> res = g.preOrder(root);
+                for (int i = 0; i < res.size (); i++)
+                    System.out.print (res.get (i) + " ");
+                System.out.print("\n");
+                t--;
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+/* A Binary Tree node 
+
+class Node {
+    int data;
+    Node left, right;
+   Node(int item)    {
+        data = item;
+        left = right = null;
+    }
+} */
+class Tree
+{
+    // Return a list containing the Preorder traversal of the given tree
+    ArrayList<Integer> preOrder(Node root)
+    {
+        // Code
+        ArrayList<Integer> al = new ArrayList<Integer>();
+        Stack<Node> st =  new Stack<>();
+        if(root == null){
+            return al;
+        }
+        st.push(root);
+        while(!st.isEmpty()){
+            Node top = st.peek();
+            al.add(top.data);
+            st.pop();
+            if(top.right != null){
+                st.push(top.right);
+            }
+            if(top.left != null){
+                st.push(top.left);
+            }
+        }
+        return al;
+    }
+    
+    
+}
