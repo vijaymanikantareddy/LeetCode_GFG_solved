8e55b046b5998251266945ad870e32453e73f4b7
Vijay Manikanta Reddy Sathi <83952736+vijaymanikantareddy@users.noreply.github.com>
2023-01-23 19:33:50 +0530

    Added solution - LeetHub

---
+++ Find position of set bit - GFG/find-position-of-set-bit.java
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            int N = Integer.parseInt(read.readLine());
+
+            Solution ob = new Solution();
+            System.out.println(ob.findPosition(N));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    static int findPosition(int N) {
+        // code here
+        if((N&(N-1)) != 0 || N < 1){
+            return -1;
+        }
+        int res = 1;
+        while(N>1){
+            N /= 2;
+            res++;
+        }
+        return res;
+    }
+};
