3cb8012882097077f572efc38e8e77c20597fcdc
Vijay Manikanta Reddy Sathi <83952736+vijaymanikantareddy@users.noreply.github.com>
2023-01-24 10:52:14 +0530

    Added solution - LeetHub

---
+++ Floor in a Sorted Array - GFG/floor-in-a-sorted-array.java
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Searching
+{
+    public static void main (String[] args)throws IOException {
+
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out=new PrintWriter(System.out);
+        int t = Integer.parseInt(read.readLine());
+        
+        while(t-- > 0)
+        {
+           String a[] = read.readLine().trim().split("\\s+");
+           int n = Integer.parseInt(a[0]);
+           long x = Long.parseLong(a[(int)1]);
+           String st[] = read.readLine().trim().split("\\s+");
+        
+           long arr[] = new long[n];
+           for(int i = 0; i < n; i++)
+           {
+               arr[i] = Long.parseLong(st[i]);
+           }
+           
+           out.println(new Solution().findFloor(arr, n, x));
+          
+        }
+        out.close();
+    }
+    
+}
+// } Driver Code Ends
+
+
+class Solution{
+    
+    // Function to find floor of x
+    // arr: input array
+    // n is the size of array
+    static int findFloor(long arr[], int n, long x)
+    {
+        int res = Integer.MIN_VALUE;
+        int ind = -1;
+        for(int i = 0 ; i < n ; i++){
+            if(arr[i] > res && arr[i] <= x){
+                res = (int) arr[i];
+                ind = i;
+            }
+        }
+        return ind;
+    }
+    
+}
