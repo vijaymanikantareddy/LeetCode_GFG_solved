2e7a294befcee4ba7a9c6f2c073dceb09b284ca9
Vijay Manikanta Reddy Sathi <83952736+vijaymanikantareddy@users.noreply.github.com>
2023-04-23 23:34:08 +0530

    Added solution - LeetHub

---
+++ Minimum Number - GFG/minimum-number.java
@@ -0,0 +1,71 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+
+class IntArray
+{
+    public static int[] input(BufferedReader br, int n) throws IOException
+    {
+        String[] s = br.readLine().trim().split(" ");
+        int[] a = new int[n];
+        for(int i = 0; i < n; i++)
+            a[i] = Integer.parseInt(s[i]);
+
+        return a;
+    }
+
+    public static void print(int[] a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+
+    public static void print(ArrayList<Integer> a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            
+            int n;
+            n = Integer.parseInt(br.readLine());
+            
+            
+            int[] arr = IntArray.input(br, n);
+            
+            Solution obj = new Solution();
+            int res = obj.minimumNumber(n, arr);
+            
+            System.out.println(res);
+            
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    public static int gcd(int a, int b){
+        return (b == 0)? a: gcd(b, a%b);
+    }
+    public static int minimumNumber(int n, int[] arr) {
+        // code here
+        int res = arr[0];
+        for(int i = 1 ; i < n ; i++){
+            res = gcd(arr[i], res);
+        }
+        return res;
+    }
+}
+        
