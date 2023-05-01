61f3c207061a48e762d241b56e337e5ad35f6a0c
Vijay Manikanta Reddy Sathi <83952736+vijaymanikantareddy@users.noreply.github.com>
2023-05-01 19:39:19 +0530

    Added solution - LeetHub

---
+++ Shuffle integers - GFG/shuffle-integers.cpp
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+	
+	
+	
+	public:
+	void shuffleArray(int arr[],int n)
+	{
+	    // Your code goes here
+	    n /= 2;
+	    for(int i = 0 ; i < n ; i++){
+	        arr[i+n] += arr[i]*10000; 
+	    }
+	    for(int i = 0 ; i < n ; i++){
+	        arr[2*i] = arr[i+n]/10000;
+	        arr[2*i+1] = arr[i+n]%10000;
+	    }
+	}
+		 
+
+};
+
+//{ Driver Code Starts.
+
+
+int main() 
+{
+   
+   
+   	int t;
+    cin >> t;
+    while (t--)
+    {
+    	int n;
+	    cin>>n;
+	    int a[n] ;
+	    for(int i=0;i<n;i++)
+	    {
+	        cin>>a[i];
+	    }
+
+
+       
+
+        Solution ob;
+        ob.shuffleArray(a, n);
+
+		for (int i = 0; i < n; i++) 
+			cout << a[i] << " ";
+    
+	
+        
+	    cout << "\n";
+	     
+    }
+    return 0;
+}
+// } Driver Code Ends
