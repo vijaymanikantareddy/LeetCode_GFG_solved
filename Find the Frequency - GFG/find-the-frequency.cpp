f6999b50bbf998bd19379fc7905346db51dacb81
Vijay Manikanta Reddy Sathi <83952736+vijaymanikantareddy@users.noreply.github.com>
2023-05-29 19:06:25 +0530

    Added solution - LeetHub

---
+++ Find the Frequency - GFG/find-the-frequency.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+//Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+/*Function to find frequency of x
+* x : element whose frequency is to be found
+* v : input vector
+*/
+int findFrequency(vector<int> v, int x){
+    // Your code here
+    int res = 0;
+    for(int ele: v){
+        res += (ele == x);
+    }
+    return res;
+}
+
+//{ Driver Code Starts.
+int main() {
+	
+	int testcase;
+	
+	cin >> testcase;
+	
+	while(testcase--){
+	    int N;
+	    cin >> N;
+	    
+	    // Declaring vector 
+	    vector<int> v;
+	        
+	    // Taking vector element input
+	    for(int i = 0;i<N;i++){
+	        int k;
+	        cin >> k;
+	        v.push_back(k);
+	    }
+	    
+	    // element whose frequency to be find
+	    int x;
+	    cin >> x;
+	    cout << findFrequency(v, x) << endl;
+	}
+	
+	return 0;
+}
+// } Driver Code Ends
