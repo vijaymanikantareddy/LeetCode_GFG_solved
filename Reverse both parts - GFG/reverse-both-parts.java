c555da2543b5bf8f43c6c8e9c52975fb15403154
Vijay Manikanta Reddy Sathi <83952736+vijaymanikantareddy@users.noreply.github.com>
2023-06-03 10:52:00 +0530

    Added solution - LeetHub

---
+++ Reverse both parts - GFG/reverse-both-parts.java
@@ -0,0 +1,112 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+
+class Node
+{
+    int data;
+    Node next;
+
+    Node(int x)
+    {
+        data = x;
+        next = null;
+    }
+
+    public static Node inputList(BufferedReader br) throws IOException
+    {
+        int n = Integer.parseInt(br.readLine().trim()); // Length of Linked List
+
+        String[] s = br.readLine().trim().split(" ");
+        Node head = new Node(Integer.parseInt(s[0])), tail = head;
+        for(int i = 1; i < s.length; i++)
+            tail = tail.next = new Node(Integer.parseInt(s[i]));
+
+        return head;
+    }
+
+    public static void printList(Node node)
+    {
+        while (node != null)
+        {
+    		System.out.print(node.data + " ");
+    		node = node.next;
+    	}
+    	System.out.println();
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            
+            Node head = Node.inputList(br);
+            
+            
+            int k;
+            k = Integer.parseInt(br.readLine());
+            
+            Solution obj = new Solution();
+            Node res = obj.reverse(head, k);
+            
+            Node.printList(res);
+            
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+/*
+
+Definition for singly Link List Node
+class Node
+{
+    int data;
+    Node next;
+
+    Node(int x){
+        data = x;
+        next = null;
+    }
+}
+
+You can also use the following for printing the link list.
+Node.printList(Node node);
+*/
+
+class Solution {
+    public static Node rev(Node head){
+        Node ptr = head, pre = null, temp;
+        while(ptr != null){
+            temp = ptr.next;
+            ptr.next = pre;
+            pre = ptr;
+            ptr = temp;
+        }
+        return pre;
+    }
+    public static Node reverse(Node head, int k) {
+        // code here
+        Node first = head, second = null, ptr = head;
+        while(k-->1){
+            ptr = ptr.next;
+        }
+        second = ptr.next;
+        ptr.next = null;
+        first = rev(first);
+        second = rev(second);
+        ptr = first;
+        while(ptr.next != null){
+            ptr = ptr.next;
+        }
+        ptr.next = second;
+        return first;
+    }
+}
+        
