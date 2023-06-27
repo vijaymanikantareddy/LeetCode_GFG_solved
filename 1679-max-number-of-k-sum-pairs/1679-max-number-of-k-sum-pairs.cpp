40ddf076ebb390c93841fdff6661c495dff0977b
Vijay Manikanta Reddy Sathi <83952736+vijaymanikantareddy@users.noreply.github.com>
2023-06-27 15:06:44 +0530

    Time: 265 ms (7.84%), Space: 74.5 MB (5.71%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int maxOperations(vector<int>& nums, int k) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
         map<int, int> mp;
         int res = 0;
         for(int i = 0 ; i < nums.size() ; i++){
