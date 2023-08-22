4e8b196b2764c15bf8bdb136290945a4abbcc3eb
Vijay Manikanta Reddy Sathi <83952736+vijaymanikantareddy@users.noreply.github.com>
2023-08-22 18:18:27 +0530

    Time: 2 ms (49.78%), Space: 6.1 MB (11.31%) - LeetHub

---
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string convertToTitle(int columnNumber) {
+        string res;
+        while(columnNumber>0)
+        {
+            int d = columnNumber%26;
+            if(d==0){
+                res = 'Z'+res;
+                columnNumber=columnNumber/26-1;
+            }
+            else
+            {   
+                res = (char)(64+d)+res;
+                columnNumber/=26;
+            }
+        }
+        return res;
+    }
+};
