763e7f9c8d6abf2e5e3f772f3227157bb39e3a1b
Vijay Manikanta Reddy Sathi <83952736+vijaymanikantareddy@users.noreply.github.com>
2023-10-27 19:13:35 +0530

    Added solution - LeetHub

---
+++ Minimum Deletions - GFG/minimum-deletions.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{
+  public:
+    int minimumNumberOfDeletions(string S) { 
+        // code here
+        string r = S;
+        int n = S.size();
+        reverse(r.begin(), r.end());
+        vector<vector<int>> dp(n+1, vector<int>(n+1, 0));
+        for(int i = 1 ; i <= n ; i++){
+            for(int j = 1 ; j <= n ; j++){
+                if(S[i-1] == r[j-1]){
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                }else{
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+        return n-dp[n][n];
+    } 
+};
+
+//{ Driver Code Starts.
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        string S;
+        cin >> S;
+        Solution obj;
+        cout << obj.minimumNumberOfDeletions(S) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
