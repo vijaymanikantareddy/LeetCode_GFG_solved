e65e768c42543f338e3e7237b561a42a7fe83701
Vijay Manikanta Reddy Sathi <83952736+vijaymanikantareddy@users.noreply.github.com>
2023-12-22 17:51:23 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Maximum Meetings in One Room/maximum-meetings-in-one-room.cpp
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+public:
+    vector<int> maxMeetings(int N,vector<int> &S,vector<int> &F){
+        vector<int> res;
+        vector<vector<int>> v;
+        for(int i = 0 ; i < N ; i++){
+            v.push_back({F[i], S[i], i+1});
+        }
+        sort(v.begin(), v.end());
+        int lastend = -1;
+        for(auto it: v){
+            if(it[1] > lastend){
+                res.push_back(it[2]);
+                lastend = it[0];
+            }
+        }
+        sort(res.begin(), res.end());
+        return res;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int> S(n),F(n);
+        for(int i=0;i<n;i++){
+            cin>>S[i];
+        }
+        for(int i=0;i<n;i++){
+            cin>>F[i];
+        }
+        Solution ob;
+        vector<int> ans=ob.maxMeetings(n,S,F);
+        for(int i=0;i<ans.size();i++){
+            cout<<ans[i];
+            if(i!=ans.size()-1){
+                cout<<" ";
+            }
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
