919d475cce9625c211a3233a91dd351a2441cd53
Vijay Manikanta Reddy Sathi <83952736+vijaymanikantareddy@users.noreply.github.com>
2024-01-11 19:11:06 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Search in Linked List/search-in-linked-list.cpp
@@ -0,0 +1,68 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node *next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+
+// } Driver Code Ends
+/* Link list node */
+/*
+struct Node
+{
+    int data;
+    Node* next;
+    Node(int x) {  data = x;  next = NULL; }
+}; */
+
+class Solution {
+  public:
+    // Function to count nodes of a linked list.
+    bool searchKey(int n, struct Node* head, int key) {
+        // Code here
+        if(head == NULL) return false;
+        struct Node* temp = head;
+        while(temp != NULL){
+            if(temp->data == key) return true;
+            temp = temp->next;
+        }
+        return false;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        int data;
+        cin >> data;
+        struct Node *head = new Node(data);
+        struct Node *tail = head;
+        for (int i = 0; i < n - 1; ++i) {
+            cin >> data;
+            tail->next = new Node(data);
+            tail = tail->next;
+        }
+
+        int key;
+        cin >> key;
+        Solution ob;
+        cout << ob.searchKey(n, head, key) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
