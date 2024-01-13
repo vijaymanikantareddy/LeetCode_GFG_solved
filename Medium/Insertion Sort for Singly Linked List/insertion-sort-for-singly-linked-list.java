ef575e8535dd87be01ceada08eaadb29cbcea93f
Vijay Manikanta Reddy Sathi <83952736+vijaymanikantareddy@users.noreply.github.com>
2024-01-13 21:26:23 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Insertion Sort for Singly Linked List/insertion-sort-for-singly-linked-list.java
@@ -0,0 +1,95 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+class Node
+    {
+        int data;
+        Node next;
+        Node(int d) {data = d; next = null; }
+    }
+class insertion
+{
+    Node head;  
+    Node tail;
+	public void addToTheLast(Node node) 
+	{
+	  if (head == null) 
+	  {
+	   head = node;
+	   tail = node;
+	  } 
+	  else 
+	  {
+	   tail.next = node;
+	   tail = node;
+	  }
+	}
+      void printList(Node head)
+    {
+        Node temp = head;
+        while (temp != null)
+        {
+           System.out.print(temp.data+" ");
+           temp = temp.next;
+        }  
+        System.out.println();
+    }
+	/* Drier program to test above functions */
+	public static void main(String args[])
+    {
+         Scanner sc = new Scanner(System.in);
+		 int t=sc.nextInt();
+		 while(t>0)
+         {
+			int n = sc.nextInt();
+			insertion llist = new insertion(); 
+			int a1=sc.nextInt();
+			Node head= new Node(a1);
+            llist.addToTheLast(head);
+            for (int i = 1; i < n; i++) 
+			{
+				int a = sc.nextInt(); 
+				llist.addToTheLast(new Node(a));
+			}
+			
+        Solution ob = new Solution();
+		head = ob.insertionSort(llist.head);
+		llist.printList(head);
+		
+        t--;		
+        }
+    }}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+/*class Node
+    {
+        int data;
+        Node next;
+        Node(int d) {data = d; next = null; }
+    }
+    */
+class Solution
+{
+    public static Node insertionSort(Node head_ref)
+    {
+        //code here
+        Node cur = head_ref;
+        while(cur.next != null){
+            Node t = cur.next;
+            while(t != null){
+                if(cur.data > t.data){
+                    int temp = cur.data;
+                    cur.data = t.data;
+                    t.data = temp;
+                }
+                t = t.next;
+            }
+            cur = cur.next;
+        }
+        return head_ref;
+    }
+}
