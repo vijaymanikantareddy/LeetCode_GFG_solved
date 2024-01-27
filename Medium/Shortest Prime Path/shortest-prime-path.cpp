b2e61bbf50ba7d5cdcb0ba7f492ac75359ff3746
Vijay Manikanta Reddy Sathi <83952736+vijaymanikantareddy@users.noreply.github.com>
2024-01-27 19:41:38 +0530

    Updated Solution - GfG to GitHub

--- Medium/Shortest Prime Path/shortest-prime-path.cpp
+++ Medium/Shortest Prime Path/shortest-prime-path.cpp
@@ -6,17 +6,18 @@ using namespace std;
 // } Driver Code Ends
 //User function Template for C++
 class Solution{   
-public:
-    bool isprime(int n){
+  public:
+    bool isprime(int n)
+    {
         if(n <= 1) return false;
         for(int i = 2 ; i*i <= n ;i++){
             if(n%i == 0) return false;
         }
         return true;
     }
-    int solve(int Num1,int Num2)
+    int shortestPath(int Num1,int Num2)
     {   
-      //code here
+        // Complete this function using prime vector
         queue<pair<string, int>> q;
         if(Num1 == Num2) return 0;
         string a = to_string(Num1);
@@ -30,17 +31,17 @@ public:
             if(s == b) return steps;
             q.pop();
             for(int i = 0 ; i < 4 ; i++){
-                char ch = s[i];
                 for(char j = '0' ; j <= '9' ; j++){
                     if(i == 0 && j == '0') continue;
+                    char ch = s[i];
                     s[i] = j;
                     int number = stoi(s);
                     if(isprime(number) && vis.find(s) == vis.end()){
                         q.push({s, steps+1});
                         vis.insert(s);
                     }
+                    s[i] = ch;
                 }
-                s[i] = ch;
             }
         }
         return -1;
@@ -51,15 +52,15 @@ public:
 //{ Driver Code Starts.
 signed main()
 {
-  int t;
-  cin>>t;
-  while(t--)
-  {
-      int Num1,Num2;
-      cin>>Num1>>Num2;
-      Solution obj;
-      int answer=obj.solve(Num1,Num2);
-      cout<<answer<<endl;
-  }
+    int t;
+    cin>>t;
+    Solution obj;
+    while(t--)
+    {
+        int Num1,Num2;
+        cin>>Num1>>Num2;
+        int answer=obj.shortestPath(Num1,Num2);
+        cout<<answer<<endl;
+    }
 }
 // } Driver Code Ends
