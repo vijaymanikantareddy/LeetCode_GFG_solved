b7e9d2caa50ec73596611698576d878a74046703
Vijay Manikanta Reddy Sathi <83952736+vijaymanikantareddy@users.noreply.github.com>
2024-03-22 19:13:58 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Diagonal sum in binary tree/diagonal-sum-in-binary-tree.cpp
@@ -0,0 +1,141 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+};
+// Utility function to create a new Tree Node
+Node* newNode(int val)
+{
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+    
+    return temp;
+}
+// Function to Build Tree
+Node* buildTree(string str)
+{   
+    // Corner Case
+    if(str.length() == 0 || str[0] == 'N')
+            return NULL;
+    
+    // Creating vector of strings from input 
+    // string after spliting by space
+    vector<string> ip;
+    
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+        
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+        
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+        
+    // Starting from the second element
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+            
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+            
+        // Get the current node's value from the string
+        string currVal = ip[i];
+            
+        // If the left child is not null
+        if(currVal != "N") {
+                
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+            
+        // For the right child
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+            
+        // If the right child is not null
+        if(currVal != "N") {
+                
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+    
+    return root;
+}
+
+
+// } Driver Code Ends
+/*Complete the function below
+Node is as follows:
+struct Node{
+    int data;
+    Node *left,*right;
+};
+*/
+
+class Solution
+{
+    public:
+    map<int, int> mp;
+    void traverse(Node* root, int diag){
+        if(root == NULL) return;
+        mp[diag] += root->data;
+        if(root->right){
+            traverse(root->right, diag);
+        }
+        if(root->left){
+            traverse(root->left, diag+1);
+        }
+    }
+    vector <int> diagonalSum(Node* root) {
+        // Add your code here
+        mp.clear();
+        traverse(root, 0);
+        vector<int> res;
+        for(auto it: mp){
+            res.push_back(it.second);
+        }
+        return res;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main()
+{
+
+    int t;
+	scanf("%d ",&t);
+    while(t--)
+    {
+        string s;
+		getline(cin,s);
+        Node* root = buildTree(s);
+        Solution obj;
+        vector <int> res = obj.diagonalSum(root);
+        for (int i : res) cout << i << " ";
+        cout<<endl;
+    }
+    return 1;
+}
+// } Driver Code Ends
