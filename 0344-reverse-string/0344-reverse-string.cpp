a14f61161372d978a98b6cc65dfa736fa2d707bf
Vijay Manikanta Reddy Sathi <83952736+vijaymanikantareddy@users.noreply.github.com>
2024-06-02 11:27:47 +0530

    Time: 24 ms (14.65%), Space: 27.1 MB (21.89%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int l=0, r = s.size()-1;
+        while(l<r){
+            char temp = s[l];
+            s[l] = s[r];
+            s[r] = temp;
+            l++;
+            r--;
+        }
+    }
+};
