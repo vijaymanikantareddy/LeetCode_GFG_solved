4ce50b9779615c2529d0cbae82e93f480436d032
Vijay Manikanta Reddy Sathi <83952736+vijaymanikantareddy@users.noreply.github.com>
2024-06-07 11:02:24 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Count Number of Nodes in a Binary Tree/count-number-of-nodes-in-a-binary-tree.cpp
@@ -0,0 +1,99 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int val;
+    Node *left;
+    Node *right;
+    Node()
+        : val(0)
+        , left(nullptr)
+        , right(nullptr) {}
+    Node(int x)
+        : val(x)
+        , left(nullptr)
+        , right(nullptr) {}
+    Node(int x, Node *left, Node *right)
+        : val(x)
+        , left(left)
+        , right(right) {}
+};
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    int res = 0;
+    void traverse(Node* root){
+        if(root == NULL) return;
+        res++;
+        if(root->left){
+            traverse(root->left);
+        }
+        if(root->right){
+            traverse(root->right);
+        }
+    }
+    int countNodes(Node* root) {
+        // Write your code here
+        traverse(root);
+        return res;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t = 1;
+    cin >> t;
+
+    // freopen ("output_gfg.txt", "w", stdout);
+
+    while (t--) {
+        // Input
+
+        int n;
+        cin >> n;
+        vector<int> v(n);
+        for (int i = 0; i < n; i++) {
+            cin >> v[i];
+        }
+        Node *root = new Node(v[0]);
+
+        int count = 0;
+        queue<Node *> q;
+        q.push(root);
+        Node *cur = NULL;
+        for (int i = 1; i < v.size(); i++) {
+            Node *node = new Node(v[i]);
+            if (count == 0) {
+                cur = q.front();
+                q.pop();
+            }
+            if (count == 0) {
+                count++;
+                cur->left = node;
+            } else {
+                count = 0;
+                cur->right = node;
+            }
+            if (v[i] >= 0) {
+                q.push(node);
+            }
+        }
+
+        Solution obj;
+        cout << obj.countNodes(root) << endl;
+
+        // cout << "~\n";
+    }
+    // fclose(stdout);
+
+    return 0;
+}
+
+// } Driver Code Ends
