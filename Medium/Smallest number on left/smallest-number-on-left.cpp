f95033afb0a3a58e2c09e48f48d0f88d306dfd80
Vijay Manikanta Reddy Sathi <83952736+vijaymanikantareddy@users.noreply.github.com>
2024-06-10 14:31:26 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Smallest number on left/smallest-number-on-left.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    vector<int> leftSmaller(int n, int a[]){
+        // code here
+        vector<int> res(n, INT_MAX);
+        stack<int> st;
+        for(int i = 0 ; i < n ; i++){
+            int te = a[i];
+            while(!st.empty() && a[st.top()] >= te){
+                st.pop();
+            }
+            res[i] = st.empty()? -1: a[st.top()];
+            st.push(i);
+        }
+        return res;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int a[n];
+        for(int i = 0;i < n;i++)
+            cin>>a[i];
+        
+        Solution ob;
+        vector<int> ans = ob.leftSmaller(n, a);
+        for(int i = 0;i < n;i++)
+            cout<<ans[i]<<" ";
+        cout<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
