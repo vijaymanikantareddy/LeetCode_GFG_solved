8b9bfee238a5d37d840b71595cf784559dfa7a53
Vijay Manikanta Reddy Sathi <83952736+vijaymanikantareddy@users.noreply.github.com>
2024-07-14 08:20:42 +0530

    Time: 668 ms (11.11%), Space: 309.1 MB (11.11%) - LeetHub

---
+++ 3500-minimum-cost-for-cutting-cake-ii/3500-minimum-cost-for-cutting-cake-ii.cpp
@@ -0,0 +1,27 @@
+using ll = long long;
+class Solution {
+public:
+    long long minimumCost(int m, int n, vector<int>& horizontalCut, vector<int>& verticalCut) {
+        vector<pair<ll,bool>> v;
+        for(int cost: horizontalCut){
+            v.push_back({cost, true});
+        }
+        for(int cost: verticalCut){
+            v.push_back({cost, false});
+        }
+        sort(v.rbegin(), v.rend());
+
+        ll hor = 1, ver = 1, res = 0;
+        for(auto cut:v){
+            if(cut.second){
+                res += (ll)cut.first*ver;
+                hor++;
+            }
+            else{
+                res += (ll)cut.first*hor;
+                ver++;
+            }
+        }
+        return res;
+    }
+};
