a8b7eedc92b64ab667010fcecedfe78f82c5ac35
Vijay Manikanta Reddy Sathi <83952736+vijaymanikantareddy@users.noreply.github.com>
2024-09-03 15:59:50 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Minimum number of deletions and insertions/minimum-number-of-deletions-and-insertions.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+	public:
+		int minOperations(string str1, string str2) 
+	{ 
+	    // Your code goes here
+	    int n = str1.size(), m = str2.size();
+	    vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+	    for(int i = 1 ; i <= n ; i++){
+	        for(int j = 1 ; j <= m ; j++){
+	            if(str1[i-1] == str2[j-1]){
+	                dp[i][j] = 1 + dp[i-1][j-1];
+	            }else{
+	                dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+	            }
+	        }
+	    }
+	    int lcs = dp[n][m];
+	    return (n - lcs) + (m - lcs);
+	} 
+};
+
+//{ Driver Code Starts.
+int main() 
+{
+   	
+   
+   	int t;
+    cin >> t;
+    while (t--)
+    {
+        string s1, s2;
+        cin >> s1 >> s2;
+
+	    Solution ob;
+	    cout << ob.minOperations(s1, s2) << "\n";
+	     
+    }
+    return 0;
+}
+
+
+// } Driver Code Ends
