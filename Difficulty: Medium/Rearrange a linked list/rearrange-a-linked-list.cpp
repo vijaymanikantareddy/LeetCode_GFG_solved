e51d66a1a104e8b2943546f478e72e9a5ccbf214
Vijay Manikanta Reddy Sathi <83952736+vijaymanikantareddy@users.noreply.github.com>
2024-09-04 19:39:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Rearrange a linked list/rearrange-a-linked-list.cpp
@@ -0,0 +1,95 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node *next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+void printList(Node *node) {
+    while (node != NULL) {
+        cout << node->data << " ";
+        node = node->next;
+    }
+    cout << "\n";
+}
+
+
+// } Driver Code Ends
+/* Node structure
+
+struct Node
+{
+    int data;
+    struct Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+*/
+
+class Solution {
+  public:
+    void rearrangeEvenOdd(Node *head) {
+        // Your Code here
+        if(head == NULL or head->next == NULL) return;
+        Node* odd = head;
+        Node* even = head->next;
+        Node* evenHead = head->next;
+        while(odd->next != NULL && even->next != NULL){
+            odd->next = even->next;
+            odd = odd->next;
+            even->next = odd->next;
+            even = even->next;
+        }
+        odd->next = evenHead;
+        return ;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+
+        // Read numbers from the input line
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Node *head = nullptr;
+        // Check if the array is empty
+        if (!arr.empty()) {
+            head = new Node(arr[0]);
+            Node *tail = head;
+            for (size_t i = 1; i < arr.size(); ++i) {
+                tail->next = new Node(arr[i]);
+                tail = tail->next;
+            }
+        }
+        Solution ob;
+        ob.rearrangeEvenOdd(head);
+        printList(head);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
