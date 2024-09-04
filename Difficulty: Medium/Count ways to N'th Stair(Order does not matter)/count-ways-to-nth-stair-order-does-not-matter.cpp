e17dc2d0d32f99cf331a9c1d7a916de63e54189a
Vijay Manikanta Reddy Sathi <83952736+vijaymanikantareddy@users.noreply.github.com>
2024-09-04 11:33:44 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Count ways to N'th Stair(Order does not matter)/count-ways-to-nth-stair-order-does-not-matter.cpp
@@ -0,0 +1,27 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+	public:
+	int nthStair(int n){
+	    //  Code here
+	    return n/2 + 1;
+	}
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n;
+		cin >> n;
+		Solution ob;
+		int ans  = ob.nthStair(n);
+		cout << ans <<"\n";
+	}
+	return 0;
+}
+// } Driver Code Ends
