9d260b48fb436e43cbeaff4c30bdf1ac4c809f62
Vijay Manikanta Reddy Sathi <83952736+vijaymanikantareddy@users.noreply.github.com>
2024-10-01 18:56:36 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Multiply two linked lists/multiply-two-linked-lists.cpp
@@ -0,0 +1,153 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+/* Linked list Node */
+struct Node {
+    int data;
+    struct Node *next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+/* Function to create a new Node with given data */
+struct Node *newNode(int data) {
+    struct Node *new_Node = new Node(data);
+
+    return new_Node;
+}
+
+Node *reverse(Node **r) {
+    Node *prev = NULL;
+    Node *cur = *r;
+    Node *nxt;
+    while (cur != NULL) {
+        nxt = cur->next;
+        cur->next = prev;
+        prev = cur;
+        cur = nxt;
+    }
+    *r = prev;
+}
+
+/* Function to insert a Node at the beginning of the Doubly Linked List */
+void push(struct Node **head_ref, int new_data) {
+    /* allocate Node */
+    struct Node *new_Node = newNode(new_data);
+
+    /* link the old list off the new Node */
+    new_Node->next = (*head_ref);
+
+    /* move the head to point to the new Node */
+    (*head_ref) = new_Node;
+}
+
+void freeList(struct Node *Node) {
+    struct Node *temp;
+    while (Node != NULL) {
+
+        temp = Node;
+        Node = Node->next;
+        free(temp);
+    }
+}
+
+
+// } Driver Code Ends
+/* Linked list node structure
+struct Node
+{
+    int data;
+    Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+
+};*/
+
+/*The method multiplies
+two  linked lists l1 and l2
+and returns their product*/
+
+/* Multiply contents of two linked lists */
+class solution {
+  public:
+    long long multiplyTwoLists(Node *first, Node *second) {
+        // code here
+        long long mod = 1e9 + 7;
+        long long a = 0, b = 0;
+        Node* f = first, *s = second;
+        while(f){
+            a = (a*10 + f->data)%mod;
+            f = f->next;
+        }
+        while(s){
+            b = (b*10 + s->data)%mod;
+            s = s->next;
+        }
+        // cout << a << " " << b << endl;
+        return (a%mod)*(b%mod)%mod;
+    }
+};
+
+//{ Driver Code Starts.
+
+// A utility function to print a linked list
+void printList(struct Node *Node) {
+    while (Node != NULL) {
+        printf("%d ", Node->data);
+        Node = Node->next;
+    }
+    printf("\n");
+}
+
+/* Driver program to test above function */
+int main(void) {
+
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+
+        struct Node *first = NULL;
+        struct Node *second = NULL;
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        for (int i = 0; i < arr.size(); i++) {
+            push(&first, arr[i]);
+        }
+        vector<int> brr;
+        string input2;
+        getline(cin, input2);
+        stringstream ss2(input2);
+        int number1;
+        while (ss2 >> number1) {
+            brr.push_back(number1);
+        }
+        for (int i = 0; i < brr.size(); i++) {
+            push(&second, brr[i]);
+        }
+        reverse(&first);
+        reverse(&second);
+        solution ob;
+        long long res = ob.multiplyTwoLists(first, second);
+        cout << res << endl;
+        freeList(first);
+        freeList(second);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
