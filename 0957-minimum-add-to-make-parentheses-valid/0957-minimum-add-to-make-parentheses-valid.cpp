613460a50a4cb5bb662afbb380f87e87ab7020c3
Vijay Manikanta Reddy Sathi <83952736+vijaymanikantareddy@users.noreply.github.com>
2024-10-09 06:58:25 +0530

    Time: 0 ms (100%), Space: 7.9 MB (32.94%) - LeetHub

--- 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.cpp
+++ 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.cpp
@@ -2,19 +2,17 @@ class Solution {
 public:
     int minAddToMakeValid(string s) {
         stack<char> st;
-        int res = 0;
-        for(int i = 0 ; i < s.size() ; i++){
-            if(s[i] == '('){
-                st.push(s[i]);
-            }
-            if(s[i] == ')'){
+        for(char ch: s){
+            if(ch == '('){
+                st.push(ch);
+            }else{
                 if(!st.empty() && st.top() == '('){
                     st.pop();
                 }else{
-                    st.push(s[i]);
+                    st.push(ch);
                 }
             }
-        }
-        return st.size();
+        }                       
+        return st.size();                         
     }
 };
