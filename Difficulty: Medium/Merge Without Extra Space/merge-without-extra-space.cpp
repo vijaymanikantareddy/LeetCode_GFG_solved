cd4fe19b65395a5cf7f010aa7fcc26e97a0194bd
Vijay Manikanta Reddy Sathi <83952736+vijaymanikantareddy@users.noreply.github.com>
2025-01-31 11:53:20 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Merge Without Extra Space/merge-without-extra-space.cpp
@@ -0,0 +1,73 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    void mergeArrays(vector<int>& a, vector<int>& b) {
+        // code here
+        int i = a.size()-1, j = 0;
+        while(i >= 0 && j < b.size()){
+            if(a[i] > b[j]){
+                swap(a[i], b[j]);
+                i--;
+                j++;
+            }else{
+                break;
+            }
+        }
+        sort(a.begin(), a.end());
+        sort(b.begin(), b.end());
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t; // Inputting the test cases
+
+    while (t--) {
+        vector<int> a, b;
+
+        // Reading the first array as a space-separated line
+        string arr1;
+        getline(cin >> ws, arr1); // Use ws to ignore any leading whitespace
+        stringstream ss1(arr1);
+        int num;
+        while (ss1 >> num) {
+            a.push_back(num);
+        }
+
+        // Reading the second array as a space-separated line
+        string arr2;
+        getline(cin, arr2);
+        stringstream ss2(arr2);
+        while (ss2 >> num) {
+            b.push_back(num);
+        }
+
+        Solution ob;
+        ob.mergeArrays(a, b);
+
+        // Output the merged result
+        for (int i = 0; i < a.size(); i++) {
+            cout << a[i] << " ";
+        }
+        cout << endl;
+        for (int i = 0; i < b.size(); i++) {
+            cout << b[i] << " ";
+        }
+        cout << "\n";
+        cout << "~\n";
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
