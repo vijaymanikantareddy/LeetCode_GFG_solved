74cb434940f90e73674ef58a4b784024be372314
Vijay Manikanta Reddy Sathi <83952736+vijaymanikantareddy@users.noreply.github.com>
2025-02-02 13:57:49 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Frequencies in a Limited Array/frequencies-in-a-limited-array.cpp
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // Function to count the frequency of all elements from 1 to N in the array.
+    vector<int> frequencyCount(vector<int>& arr) {
+        // code here
+        int n = arr.size();
+        vector<int> fre(n, 0);
+        for(int it: arr){
+            fre[it-1]++;
+        }
+        return fre;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t; // Number of test cases
+    cin >> t;
+    cin.ignore(); // Ignore the newline after the integer input
+    while (t--) {
+        vector<int> a;
+        string input;
+
+        // Input format: first number n followed by the array elements
+        getline(cin, input);
+        stringstream ss(input);
+        int num;
+        while (ss >> num)
+            a.push_back(num); // Read the array elements from input string
+
+        Solution obj;
+        vector<int> result = obj.frequencyCount(a);
+
+        // Print the result in the required format
+        if (result.empty()) {
+            cout << "[]"; // Print empty brackets if no duplicates are found
+        } else {
+            for (int i = 0; i < result.size(); i++) {
+                if (i != 0)
+                    cout << " ";
+                cout << result[i];
+            }
+        }
+        cout << endl; // Ensure new line after each test case output
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
