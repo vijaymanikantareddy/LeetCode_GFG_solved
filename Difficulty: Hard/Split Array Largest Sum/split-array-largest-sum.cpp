bf4d040965c30f208dbdbbf2afc8761fe4199569
Vijay Manikanta Reddy Sathi <83952736+vijaymanikantareddy@users.noreply.github.com>
2025-06-29 08:15:33 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Split Array Largest Sum/split-array-largest-sum.cpp
+++ Difficulty: Hard/Split Array Largest Sum/split-array-largest-sum.cpp
@@ -1,13 +1,6 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 class Solution {
   public:
-    int fun(int arr[], int givenTime, int n){
+     int fun(vector<int>& arr, int givenTime, int n){
         int t = 0, cnt = 1;
         for(int i = 0 ; i < n ; i++){
             if(t + arr[i] <= givenTime){
@@ -19,17 +12,17 @@ class Solution {
         }
         return cnt;
     }
-    
-    int splitArray(int arr[] ,int N, int K) {
+    int splitArray(vector<int>& arr, int k) {
         // code here
         int low = 0, high = 0;
-        for(int i = 0 ; i < N ; i++) {
+        int n = arr.size();
+        for(int i = 0 ; i < n ; i++) {
             low = max(low, arr[i]);
             high += arr[i];
         }
         while(low <= high){
             int mid = low + (high - low)/2;
-            if(fun(arr, mid, N) <= K){
+            if(fun(arr, mid, n) <= k){
                 high = mid - 1;
             }else{
                 low = mid + 1;
@@ -38,28 +31,3 @@ class Solution {
         return low;
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int N, K;
-        
-        cin>>N>>K;
-        int arr[N];
-        
-        for(int i=0 ; i<N ; i++)
-            cin>>arr[i];
-
-        Solution ob;
-        cout<<ob.splitArray(arr,N,K);
-        cout<<"\n";
-    
-cout << "~" << "\n";
-}
-    return 0;
-}
-// } Driver Code Ends
