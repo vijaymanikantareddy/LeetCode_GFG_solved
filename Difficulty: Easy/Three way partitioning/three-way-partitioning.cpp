fe5355743ae13d95fcdd5d61b7ce4f7ab6f8e641
Vijay Manikanta Reddy Sathi <83952736+vijaymanikantareddy@users.noreply.github.com>
2025-08-16 08:31:55 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Three way partitioning/three-way-partitioning.cpp
@@ -0,0 +1,9 @@
+class Solution {
+  public:
+    // Function to partition the array around the range such
+    // that array is divided into three parts.
+    void threeWayPartition(vector<int>& arr, int a, int b) {
+        // code here
+        sort(arr.begin(), arr.end());
+    }
+};
