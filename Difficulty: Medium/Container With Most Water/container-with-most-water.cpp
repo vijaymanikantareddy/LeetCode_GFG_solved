bede45d0aa81ba15ca5bcca8e888d9eaca25675b
Vijay Manikanta Reddy Sathi <83952736+vijaymanikantareddy@users.noreply.github.com>
2025-08-31 07:08:59 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Container With Most Water/container-with-most-water.cpp
+++ Difficulty: Medium/Container With Most Water/container-with-most-water.cpp
@@ -1,12 +1,4 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-
-using namespace std;
-
-
-// } Driver Code Ends
 class Solution {
-
   public:
     int maxWater(vector<int> &arr) {
         // code here
@@ -23,30 +15,3 @@ class Solution {
         return mx;
     }
 };
-
-//{ Driver Code Starts.
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-        vector<int> arr;
-        string input;
-
-        // Read first array
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-
-        Solution ob;
-        int res = ob.maxWater(arr);
-
-        cout << res << endl << "~" << endl;
-    }
-    return 0;
-}
-
-// } Driver Code Ends
