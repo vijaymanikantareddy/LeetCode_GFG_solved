603740ee9f88a950b3b16c4570ca39a1d3cc5c0d
Vijay Manikanta Reddy Sathi <83952736+vijaymanikantareddy@users.noreply.github.com>
2025-10-04 10:52:40 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Rearrange characters/rearrange-characters.cpp
+++ Difficulty: Medium/Rearrange characters/rearrange-characters.cpp
@@ -1,96 +1,32 @@
-//{ Driver Code Starts
-#include<bits/stdc++.h>
-using namespace std;
-const int MAX_CHAR = 26;
-
-// } Driver Code Ends
-class Solution
-{
-    public:
-    string rearrangeString(string str)
-    {
-        //code here
+class Solution {
+  public:
+    string rearrangeString(string s) {
+        // code here
         unordered_map<char, int> mp;
-        string res(str.size(), '-');
-        for(char ch: str) mp[ch]++;
+        for(auto it: s){
+            mp[it]++;
+        }
         vector<pair<int, char>> vc;
         for(auto it: mp){
-            if(it.second > (str.size()+1)/2)
-                return "-1";
+            if(it.second > (s.size() + 1)/2){
+                return "";
+            }
             vc.push_back({it.second, it.first});
         }
+        
         sort(vc.rbegin(), vc.rend());
+        
+        string res(s.size(), '-');
         int index = 0;
         for(auto it: vc){
             for(int i = 0 ; i < it.first ; i++){
                 res[index] = it.second;
                 index += 2;
-                if(index >= str.size()){
+                if(index >= s.size()){
                     index = 1;
                 }
             }
         }
         return res;
     }
-    
 };
-
-//{ Driver Code Starts.
-int main()
-{
-	int t;
-	cin>>t;
-	while(t--)
-	{
-	    string str;
-	    cin>>str;
-	    Solution ob;
-	    string str1=ob.rearrangeString(str);
-	    int flag=1;
-	    int c[26] = {0};
-	    for(int i=0; i<str.length(); i++)
-	        c[str[i]-'a']+=1;
-	    int f = 0;
-	    int x = (str.length()+1)/2;
-	    for(int i=0; i<26; i++)
-	    {
-	        if(c[i]>x)
-	            f = 1;
-	    }
-	    if(f)
-	    {
-	        if(str1=="-1")
-	            cout<<0<<endl;
-	        else
-	            cout<<1<<endl;
-	    }
-	    else
-	    {
-    	    int a[26] = {0};
-    	    int b[26] = {0};
-    	    for(int i=0; i<str.length(); i++)
-    	        a[str[i]-'a']+=1;
-    	    for(int i=0; i<str1.length(); i++)
-    	        b[str1[i]-'a']+=1;
-    	        
-    	    for(int i=0; i<26; i++)
-    	        if(a[i]!=b[i])
-    	            flag = 0;
-    	    
-    	    for(int i=0;i<str1.length();i++)
-    	    {
-    		    if(i>0)
-    		        if(str1[i-1]==str1[i])
-    			        flag=0;
-    	    }
-    	    if(flag==1)
-    		    cout<<"1\n";
-    	    else
-    	        cout<<"0\n";
-	    }
-	
-cout << "~" << "\n";
-}
-    return 0;
-}
-// } Driver Code Ends
