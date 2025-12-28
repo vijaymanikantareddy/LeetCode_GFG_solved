59b5a3255c0966ad5225fcc2efde925b5937913f
Vijay Manikanta Reddy Sathi <83952736+vijaymanikantareddy@users.noreply.github.com>
2025-12-28 17:13:23 +0530

    Time: 67 ms (11.91%), Space: 104.8 MB (20.75%) - LeetHub

--- 2895-minimum-processing-time/2895-minimum-processing-time.cpp
+++ 2895-minimum-processing-time/2895-minimum-processing-time.cpp
@@ -5,14 +5,10 @@ public:
         sort(tasks.rbegin(), tasks.rend());
         int res = 0;
         int it = 0;
-        vector<int> t;
         for(int i = 0 ; i < processorTime.size() ; i++){
             int curr = processorTime[i] + max({tasks[it], tasks[it + 1], tasks[it + 2], tasks[it + 3]});
             it += 4;
-            t.push_back(curr);
-        }
-        for(auto ele: t){
-            res = max(res, ele);
+            res = max(res, curr);
         }
         return res;
     }
