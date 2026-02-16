5858b7b4d9446e77c84f58423d692f5b217f715e
Vijay Manikanta Reddy Sathi <83952736+vijaymanikantareddy@users.noreply.github.com>
2026-02-16 22:54:33 +0530

    Time: 0 ms (100%), Space: 8.2 MB (46.07%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        int ans=0;
+        for(int i=0 ; i<32 ; i++)
+        {
+            ans<<=1;
+            ans = ans|(n&1);
+            n>>=1;
+        }
+        return ans;
+    }
+};
