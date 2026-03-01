c6603186176cea4d6ff85b2dad78950ac14987b3
Vijay Manikanta Reddy Sathi <83952736+vijaymanikantareddy@users.noreply.github.com>
2026-03-01 19:34:18 +0530

    Time: 0 ms (100%), Space: 8.8 MB (53.19%) - LeetHub

--- 3856-trim-trailing-vowels/3856-trim-trailing-vowels.cpp
+++ 3856-trim-trailing-vowels/3856-trim-trailing-vowels.cpp
@@ -4,10 +4,9 @@ public:
         return (ch == 'a' or ch == 'e' or ch == 'i' or ch == 'o' or ch == 'u');
     }
     string trimTrailingVowels(string s) {
-        int last = s.size() - 1;
-        while(last >= 0 && isvow(s[last])){
-            last--;
+        while(s.size() && isvow(s.back())){
+            s.pop_back();
         }
-        return s.substr(0, last + 1);
+        return s;
     }
 };
