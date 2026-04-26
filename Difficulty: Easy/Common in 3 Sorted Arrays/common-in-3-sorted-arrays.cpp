d6c903ffb593631e276cbae7d5875f9d6e288366
Vijay Manikanta Reddy Sathi <83952736+vijaymanikantareddy@users.noreply.github.com>
2026-04-26 06:47:55 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Common in 3 Sorted Arrays/common-in-3-sorted-arrays.cpp
+++ Difficulty: Easy/Common in 3 Sorted Arrays/common-in-3-sorted-arrays.cpp
@@ -1,18 +1,7 @@
-//{ Driver Code Starts
-// here
-
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 class Solution {
   public:
-    // Function to find common elements in three arrays.
-    vector<int> commonElements(vector<int> &arr1, vector<int> &arr2,
-                               vector<int> &arr3) {
-        // Code Here
+    vector<int> commonElements(vector<int> &arr1, vector<int> &arr2, vector<int> &arr3) {
+        // code here
         unordered_map<int, int> mp;
         unordered_map<int, int> temp;
         vector<int> res;
@@ -37,44 +26,3 @@ class Solution {
         return res;
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-        vector<int> arr, brr, crr;
-        string input1;
-        getline(cin, input1);
-        stringstream ss1(input1);
-        int number1;
-        while (ss1 >> number1) {
-            arr.push_back(number1);
-        }
-        string input2;
-        getline(cin, input2);
-        stringstream ss2(input2);
-        int number2;
-        while (ss2 >> number2) {
-            brr.push_back(number2);
-        }
-        string input3;
-        getline(cin, input3);
-        stringstream ss3(input3);
-        int number3;
-        while (ss3 >> number3) {
-            crr.push_back(number3);
-        }
-        Solution ob;
-        vector<int> res = ob.commonElements(arr, brr, crr);
-        if (res.size() == 0)
-            cout << -1;
-        for (int i = 0; i < res.size(); i++)
-            cout << res[i] << " ";
-        cout << endl << "~\n";
-    }
-}
-// } Driver Code Ends
